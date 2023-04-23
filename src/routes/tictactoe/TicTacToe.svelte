34fda822226b52119fe9a98171b73fc64b5ee6c1
0xcacti <0xcacti@gmail.com>
2023-04-23 02:14:36 -0400

    get tictactoe board displayed

--- src/routes/tictactoe/TicTacToe.svelte
+++ src/routes/tictactoe/TicTacToe.svelte
@@ -1,44 +1,41 @@
-<div class="gameboard-container border" style="border; 4px solid green;">
-  <!-- <div class="board-container">
-    <div class="crossVerticalLeft" />
-    <div class="crossVerticalRight" />
-    <div class="crossHorizontalTop" />
-    <div class="crossHorizontalBottom" />
-    <div class="o00" />
-    <div class="o01" />
-    <div class="o02" />
-    <div class="o10" />
-    <div class="o11" />
-    <div class="o12" />
-    <div class="o20" />
-    <div class="o21" />
-    <div class="o22" />
-    <div class="x00Left" />
-    <div class="x00Right" />
-    <div class="x01Left" />
-    <div class="x01Right" />
-    <div class="x02Left" />
-    <div class="x02Right" />
-    <div class="x10Left" />
-    <div class="x10Right" />
-    <div class="x11Left" />
-    <div class="x11Right" />
-    <div class="x12Left" />
-    <div class="x12Right" />
-    <div class="x20Left" />
-    <div class="x20Right" />
-    <div class="x21Left" />
-    <div class="x21Right" />
-    <div class="x22Left" />
-    <div class="x22Right" />
-  </div> -->
-</div>
+<script>
+  import "./tictactoe.css";
+</script>
 
-<style>
-  :root {
-    --a: #f9f1ed;
-    --b: #e4c5b7;
-    --c: #f2e2db;
-    --d: #c88b6f;
-    --e: #377490;
-  }
+<div class="gameboard-container">
+  <div class="flex align-center border">
+    <div class="board-container border">
+      <div class="crossVerticalLeft" />
+      <div class="crossVerticalRight" />
+      <div class="crossHorizontalTop" />
+      <div class="crossHorizontalBottom" />
+      <div class="o00" />
+      <div class="o01" />
+      <div class="o02" />
+      <div class="o10" />
+      <div class="o11" />
+      <div class="o12" />
+      <div class="o20" />
+      <div class="o21" />
+      <div class="o22" />
+      <div class="x00Left" />
+      <div class="x00Right" />
+      <div class="x01Left" />
+      <div class="x01Right" />
+      <div class="x02Left" />
+      <div class="x02Right" />
+      <div class="x10Left" />
+      <div class="x10Right" />
+      <div class="x11Left" />
+      <div class="x11Right" />
+      <div class="x12Left" />
+      <div class="x12Right" />
+      <div class="x20Left" />
+      <div class="x20Right" />
+      <div class="x21Left" />
+      <div class="x21Right" />
+      <div class="x22Left" />
+      <div class="x22Right" />
+    </div>
+  </div>
+</div>
