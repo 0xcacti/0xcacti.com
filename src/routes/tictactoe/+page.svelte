7d83245979dc67ea70e754656a562e32eb1e973c
0xcacti <0xcacti@gmail.com>
2023-04-26 21:17:18 -0400

    saving, I fear we have to pivot to react

--- src/routes/tictactoe/+page.svelte
+++ src/routes/tictactoe/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
   import TicTacToe from "./rendering/TicTacToe.svelte";
   import Controler from "./gameplay/Controler.svelte";
+  import ConnectionManager from "./ConnectionManager.svelte";
 </script>
 
 <div class="game">
+  <ConnectionManager />
   <TicTacToe />
   <Controler />
 </div>
--- src/routes/tictactoe/gameplay/Controler.svelte
+++ src/routes/tictactoe/gameplay/Controler.svelte
@@ -1,11 +1,12 @@
 <script>
   import { writable } from "svelte/store";
   import BaseCard from "./BaseCard.svelte";
-  import "./tabs.css";
+
+  import "@rainbow-me/rainbowkit/styles.css";
 
   let cardID = writable(0);
   let activeCardColor = "bg-green-300";
-  let inactiveCardColor = "bg-inherit";
+  let inactiveCardColor = "bg-green-100";
 
   function onClickFindGame() {
     $cardID = 0;
@@ -18,34 +19,19 @@
 
 <div class="gameplay-container flex flex-col justify-center h-2/3">
   <div class="flex w-full justify-start">
-    <ul class="tabs group">
-      <li class="active">
-        <button
-          class="rounded-b-none bg-blue-100 {$cardID === 0
-            ? activeCardColor
-            : inactiveCardColor}"
-          on:click={onClickFindGame}>Find Game</button
-        >
-      </li>
-      <li>
-        <button
-          class="rounded-b-none {$cardID === 0
-            ? inactiveCardColor
-            : activeCardColor}"
-          on:click={onClickNewGame}>New Game</button
-        >
-      </li>
-      <li>
-        <button
-          class="rounded-b-none bg-blue-100 {$cardID === 0
-            ? activeCardColor
-            : inactiveCardColor}"
-          on:click={onClickFindGame}>Play!</button
-        >
-      </li>
-    </ul>
+    <button
+      class="rounded-b-none bg-blue-100 {$cardID === 0
+        ? activeCardColor
+        : inactiveCardColor}"
+      on:click={onClickFindGame}>Find Game</button
+    >
 
-    <div class="flex-grow border-b border-indigo-600" />
+    <button
+      class="rounded-b-none {$cardID === 0
+        ? inactiveCardColor
+        : activeCardColor}"
+      on:click={onClickNewGame}>New Game</button
+    >
   </div>
   <BaseCard {cardID} />
 </div>
