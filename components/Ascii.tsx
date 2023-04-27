8b89bb1e3fdd6e636eeacaaebb7eb0e4eb4221ca
0xcacti <0xcacti@gmail.com>
2023-04-27 02:26:46 -0400

    I have successfully recreated the home page

---
+++ components/Ascii.tsx
@@ -0,0 +1,25 @@
+import "../styles/Ascii.module.css";
+const Ascii = () => {
+    return (
+        
+            <pre className="preWrapper">
+            {`
+    |\\_ /|   
+( o  o )
+> . </
+|    \\
+ |  |  \\
+   |  |   | 
+         (_(_f_, ) 0xcacti
+      //
+    ((
+       ))  
+    ((
+     v 
+            `}
+            </pre>
+
+    );
+};
+
+export default Ascii;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,42 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import type { NextPage } from 'next';
-import Head from 'next/head';
+import type { NextPage } from "next";
+import Ascii from "../components/Ascii";
+import Head from "next/head";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>0xcacti webby</title>
-        <meta
-          content="Personal Website of 0xcacti"
-          name="meow"
-        />
-        <link href="/favicon.ico" rel="icon" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>0xcacti webby</title>
+                <meta content="Personal Website of 0xcacti" name="meow" />
+                <link href="/favicon.ico" rel="icon" />
+            </Head>
 
-      <main >
-        <h1 className="bg-green-300">poop</h1>
-        <ConnectButton />
+            <main>
+                <div className="mb-10">
+                    <div className="container flex justify-between px-6 py-8 mx-auto">
+                        <Link href="/">
+                            <a className="text-xl font-bold text-gray-800 md:text-2xl hover:text-blue-400">
+                                0xcacti
+                            </a>
+                        </Link>
 
-      </main>
-
-    </div>
-  );
+                        <div className="flex sm:mt-1 mt-2 space-y-2 align-middle">
+                            <a
+                                className="text-gray-800 hover:text-blue-400"
+                                href="/tictactoe"
+                            >
+                                TicTacToe
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="text-teal-500 text-center border">
+                    <Ascii />
+                </div>
+            </main>
+        </div>
+    );
 };
 
 export default Home;
