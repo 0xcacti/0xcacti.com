9c7a888c67fe29e10a6ad6096051d40bb9f39dfe
0xcacti <0xcacti@gmail.com>
2023-05-01 22:23:09 -0400

    saving flex box, converting to grid

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -14,29 +14,44 @@ export default function Projects() {
                 <ConnectButton />
             </div>
 
-            <div className="w-screen flex-col border border-indigo-600 space-y-5 py-12 px-32">
+            <div className="w-screen flex-col flex-wrap border border-indigo-600 space-y-5 py-12 px-32">
                 <div className="w-full flex space-x-5 flex-row border border-green-300 justify-start">
                     <div className="flex flex-col justify-apart">
-                        <div className="h-80 w-64 rounded-lg bg-red group-hover:opacity-75 relative pt-8 text-center">
-                            <div>
-                                <div className={styles.boardContainer}>
-                                    <div
-                                        className={styles.crossVerticalLeft}
-                                    ></div>
-                                    <div
-                                        className={styles.crossVerticalRight}
-                                    ></div>
-                                    <div
-                                        className={styles.crossHorizontalTop}
-                                    ></div>
-                                    <div
-                                        className={styles.crossHorizontalBottom}
-                                    ></div>
-                                </div>
-                                <h1 className="pt-4 hidden hoverText ">
-                                    TicTacToe
-                                </h1>
+                        <div className="h-80 w-64 rounded-lg bg-red group-hover:opacity-75  pt-8 text-center">
+                            <div className={styles.boardContainer}>
+                                <div className={styles.crossVerticalLeft}></div>
+                                <div
+                                    className={styles.crossVerticalRight}
+                                ></div>
+                                <div
+                                    className={styles.crossHorizontalTop}
+                                ></div>
+                                <div
+                                    className={styles.crossHorizontalBottom}
+                                ></div>
                             </div>
+                            <h1 className="pt-4 hidden hoverText ">
+                                TicTacToe
+                            </h1>
+                        </div>
+                    </div>
+
+                    <div className="h-80 w-64 rounded-lg bg-blue  group-hover:opacity-75  pt-8 text-center">
+                        <div className={styles.boardContainer}>
+                            <div className={styles.crossVerticalLeft}></div>
+                            <div className={styles.crossVerticalRight}></div>
+                            <div className={styles.crossHorizontalTop}></div>
+                            <div className={styles.crossHorizontalBottom}></div>
+                        </div>
+                        <h1 className="pt-4 hidden hoverText ">TicTacToe</h1>
+                    </div>
+
+                    <div className="h-80 w-64 rounded-lg bg-blue  group-hover:opacity-75">
+                        <div className="container">
+                            <div className="crossVerticalLeft"></div>
+                            <div className="crossVerticalRight"></div>
+                            <div className="crossHorizontalTop"></div>
+                            <div className="crossHorizontalBottom"></div>
                         </div>
                     </div>
 
@@ -48,8 +63,23 @@ export default function Projects() {
                             <div className="crossHorizontalBottom"></div>
                         </div>
                     </div>
+                    <div className="h-80 w-64 rounded-lg bg-blue  group-hover:opacity-75">
+                        <div className="container">
+                            <div className="crossVerticalLeft"></div>
+                            <div className="crossVerticalRight"></div>
+                            <div className="crossHorizontalTop"></div>
+                            <div className="crossHorizontalBottom"></div>
+                        </div>
+                    </div>
+                    <div className="h-80 w-64 rounded-lg bg-blue  group-hover:opacity-75">
+                        <div className="container">
+                            <div className="crossVerticalLeft"></div>
+                            <div className="crossVerticalRight"></div>
+                            <div className="crossHorizontalTop"></div>
+                            <div className="crossHorizontalBottom"></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-full flex-row border border-green-300 justify-start"></div>
             </div>
         </div>
     );
