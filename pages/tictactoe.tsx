29fec3aaa5517570e2499e2a9d0b7055aab7b3c3
0xcacti <0xcacti@gmail.com>
2023-05-06 02:56:49 -0400

    made progress towards writing to contract, but I must go to bed.

--- pages/tictactoe.tsx
+++ pages/tictactoe.tsx
@@ -3,12 +3,27 @@ import { useCallback, useState } from "react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import Link from "next/link";
 import TicTacToeBoard from "../components/TicTacToeBoard";
+import contractConfig from "../contracts/TicTacToe.json";
+
+import { erc721ABI, useContractWrite, usePrepareContractWrite } from "wagmi";
 
 const TicTacToe: NextPage = () => {
     const [tokenURI, setTokenURI] = useState(0);
     const refreshData = useCallback(() => {
         setTokenURI((tokenURI) => tokenURI + 1);
     }, []);
+
+    const { config } = usePrepareContractWrite({
+        address: contractConfig.address as `0x${string}`,
+        abi: contractConfig.abi,
+        functionName: "createNewGame",
+        args: [
+            "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+            "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+        ],
+    });
+
+    const { data, isLoading, isSuccess, write } = useContractWrite(config);
 
     return (
         <div className="bg-white border min-h-screen">
@@ -31,7 +46,20 @@ const TicTacToe: NextPage = () => {
                     />
                 </div>
                 <div className="flex flex-col space-y-16 justify-center text-center p-5 h-2/3">
-                    <button onClick={refreshData}>
+                    <button
+                        disabled={!write}
+                        onClick={() => {
+                            console.log(write);
+                            write?.();
+                        }}
+                    >
+                        {isLoading && (
+                            <div> console.log(JSON.stringify(data));</div>
+                        )}
+                        {isLoading && <div>Check Wallet</div>}
+                        {isSuccess && (
+                            <div>Transaction: {JSON.stringify(data)}</div>
+                        )}
                         <div className="bg-blue p-2 rounded-lg">
                             <h1 className="text-xl font-bold">New Game</h1>
                         </div>
