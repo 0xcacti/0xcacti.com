647c63b63251d50b607d4ff35236cbfd8ce1eb29
0xcacti <0xcacti@gmail.com>
2025-02-08 01:34:28 -0500

    add a sick ass favicon

--- src/components/base.lisp
+++ src/components/base.lisp
@@ -7,6 +7,17 @@
        (:meta :charset "utf-8")
        (:meta :name "viewport" :content "width=device-width, initial-scale=1.0")
        (:title ,title)
+
+       ;; favicon setup
+       (:link :rel "icon" :type "image/x-icon" :href "/static/images/favicon.ico")
+       (:link :rel "icon" :type "image/png" :sizes "32x32" :href "/static/images/favicon-32x32.png")
+       (:link :rel "icon" :type "image/png" :sizes "16x16" :href "/static/images/favicon-16x16.png")
+
+       ;; Mobile/device specific icons
+       (:link :rel "apple-touch-icon" :sizes "180x180" :href "/static/images/apple-touch-icon.png")
+       (:link :rel "manifest" :href "/static/images/site.webmanifest")
+        
+       ;; scripts
        (:script :src "/static/htmx.min.js")
        (:script :src "https://cdn.tailwindcss.com"))
       (:body :class "min-h-screen flex flex-col"
