1400e7b111572525b5252f75c478dcb755e27e98
0xcacti <0xcacti@gmail.com>
2025-02-13 21:28:47 -0500

    save

--- scripts/start.lisp
+++ scripts/start.lisp
@@ -1,4 +1,7 @@
-(ql:quickload :0xcacti-website)  
+(ql:quickload :0xcacti-website)
 (in-package :0xcacti-website)
-
 (start-app :port 3000)
+(sb-thread:join-thread 
+  (find-if (lambda (th) 
+             (search "hunchentoot" (sb-thread:thread-name th)))
+           (sb-thread:list-all-threads)))
