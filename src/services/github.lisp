9cb5b5a40164a3c895f343efd874c2dcc0e7093d
0xcacti <0xcacti@gmail.com>
2025-10-30 07:41:33 -0400

    fix my contribution chart actually this time

--- src/services/github.lisp
+++ src/services/github.lisp
@@ -25,9 +25,9 @@
   (let* ((token (config:get-github-token config:*config*))
          (start-time (format nil "~A-01-01T00:00:00Z" year))
          (end-time (format nil "~A-12-31T23:59:59Z" year))
-         (query "query($username: String!, $from: DateTime, $to: DateTime, $includePrivate: Boolean!) {
+         (query "query($username: String!, $from: DateTime, $to: DateTime) {
                   user(login: $username) {
-                    contributionsCollection(from: $from, to: $to, includePrivateContributions: $includePrivate) {
+                    contributionsCollection(from: $from, to: $to) {
                       contributionCalendar {
                         totalContributions
                         weeks {
@@ -44,8 +44,7 @@
                    `(("query" . ,query)
                      ("variables" . (("username" . ,username)
                                    ("from" . ,start-time)
-                                   ("to" . ,end-time)
-                                   ("includePrivate" . t))))))
+                                   ("to" . ,end-time))))))
          (response (dk:http-request "https://api.github.com/graphql"
                     :method :post
                     :content-type "application/json"
