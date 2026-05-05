91c2c16e13960926d5e18b5c2fca69dac18fcbcb
0xcacti <0xcacti@gmail.com>
2026-05-05 19:39:20 -0400

    save the home

--- src/routes/home.lisp
+++ src/routes/home.lisp
@@ -5,13 +5,11 @@
   (components:with-base-page (:title "0xcacti")
     (:div :class "container mx-auto flex flex-col justify-between items-center gap-3 p-2"
       
-     (:div :class "w-full pt-3 flex-1 grid grid-cols-1 lg:grid-cols-2 gap-3 h-[555px] min-h-0" 
-      (:div :class "flex flex-col justify-start rounded-lg p-4 border border-gray-300 overflow-y-auto min-h-0 h-full" 
+     (:div :class "w-full pt-3 grid grid-cols-1 lg:grid-cols-2 gap-3 h-[555px] min-h-0 overflow-hidden"
+      (:div :class "flex flex-col justify-start rounded-lg p-4 border border-gray-300 min-h-0 h-full overflow-hidden"
        (:div :class "mb-8 text-2xl font-light text-black"
           "Recent Projects")
-       (:div 
-        :class "grid gap-4 sm:gap-6"
-
+       (:div :class "grid gap-4 sm:gap-6 overflow-y-auto min-h-0 flex-1 pr-2"
           (components:project
             :name "core-utils"
             :link "https://github.com/0xcacti/core-utils"
